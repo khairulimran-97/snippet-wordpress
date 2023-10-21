25b3e8a1f6589a04dafd3f024d76fc319b3a594a
Khairul Imran <105085586+khairulimran-97@users.noreply.github.com>
2023-10-22 07:13:56 +0800

    Create change-default-elementor-form.php

---
+++ change-default-elementor-form.php
@@ -0,0 +1,12 @@
+<?php
+
+add_filter( 'option_admin_email', function( $value ) {
+    $caller = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 4 );
+    if ( isset( $caller[3] ) && isset( $caller[3]['file'] ) && false !== strpos( $caller[3]['file'], 'forms/actions/email') ) {
+        //this is most likly comming from elementor pro forms module
+        //so you need to return the client's email 
+        return 'other@email.com';
+    }
+    // default value
+    return $value;
+} );
